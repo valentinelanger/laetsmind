dae7ec0c823abd0fef09045ed3525f60469e524e
Valentine Langer <valentine.langer@gmail.com>
2017-07-27 12:59:46 +0200

    second try for scroll - not working for the offset().top on line 40 in parallax.js

--- source/javascripts/scroll.js
+++ source/javascripts/scroll.js
@@ -1,12 +1,36 @@
-$(function() {
-    var dot = $(".dot");
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
+function pagination() {
 
-        if (scroll >= 500) {
-            dot.removeClass('dot').addClass("active");
-        } else {
-            dot.removeClass("dot").addClass('active');
-        }
-    });
+  var offset = $(document).scrollTop(),
+      windowHeight = $(window).height(),
+      body = $('body');
+
+  switch (true) {
+
+    case (offset > (windowHeight * 3.75)):
+      body.removeClass().addClass('page-5');
+      break;
+    case (offset > (windowHeight * 2.75)):
+      body.removeClass().addClass('page-4');
+      break;
+    case (offset > (windowHeight * 1.75)):
+      body.removeClass().addClass('page-3');
+      break;
+    case (offset > (windowHeight * .75)):
+      body.removeClass().addClass('page-2');
+      break;
+    case (offset < windowHeight):
+      body.removeClass().addClass('page-1');
+      break;
+    default:
+      body.removeClass().addClass('page-1');
+   }
+}
+
+pagination();
+
+$(document).scroll(function() {
+  pagination();
 });
+
+
+
