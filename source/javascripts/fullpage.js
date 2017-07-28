b6cf5c3f7214d3abcf6751e99fd836c3216beede
kevcha <kevin.chavanne@gmail.com>
2017-07-28 12:47:18 +0200

    Add pagination with bullets

--- source/javascripts/fullpage.js
+++ source/javascripts/fullpage.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    anchors: ['section-1', 'section-2', 'section-3', 'section-4', 'section-5'],
+    menu: '#menu'
+  });
 });
